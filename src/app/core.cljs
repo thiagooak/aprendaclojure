a3cdec1812cb7e96760045fe3a2fc768eb3500b9
Thiago Carvalho <thiago.carvalho2@nubank.com.br>
2022-10-18 19:00:04 -0300

    Add Celcius to Fahrenheit Converter

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,5 +1,39 @@
 (ns app.core
-  (:require [reagent.dom :as rd]))
+  (:require [reagent.core :as r]
+            [reagent.dom :as rd]))
+
+(defn celcius-to-fahrenheit [c]
+  (+ 32 (* 9 (/ c 5))))
+
+(defn calc-temp [c]
+  (let [f (celcius-to-fahrenheit c)]
+    ;; @TODO choose correct emoji
+    (hash-map :celcius c :fahrenheit f :emoji "🥵")))
+
+(def temp-data (r/atom (calc-temp 0)))
+
+(defn slider [value min max]
+  [:input {:type "range" :value value :min min :max max
+           :style {:width "100%"}
+           :on-change (fn [e]
+                        (let [new-value (js/parseInt (.. e -target -value))]
+                          (swap! temp-data
+                                 (fn []
+                                   (calc-temp new-value)))))}])
+
+(defn temp-component []
+  (let [{:keys [celcius fahrenheit emoji]} @temp-data]
+    [:div
+     [:h3 "Temperature Converter"]
+     [:div
+      "Celcius: " celcius "°C"
+      [slider celcius -100 +100]]
+     [:div
+      "Fahrenheit: " fahrenheit "°F"
+      ;; [slider fahrenheit -100 +100] @TODO add F sliders
+      ]
+     [:div
+      "Emoji: " emoji]]))
 
 (defn some-component []
   [:div {:class "max-w-4xl mx-auto min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8"}
@@ -11,7 +45,7 @@
 
 (defn ^:dev/after-load start
   []
-  (rd/render [some-component]
+  (rd/render [temp-component]
              (.getElementById js/document "app")))
 
 (defn init []
