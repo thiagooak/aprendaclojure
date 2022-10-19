96607d9fde3403ca21c8cad99391e8b63a828637
Thiago Carvalho <thiago.carvalho2@nubank.com.br>
2022-10-19 09:59:39 -0300

    Add tests and refactoring

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -3,29 +3,36 @@
             [reagent.dom :as rd]))
 
 (defn c-to-f [c]
-  (Math/round (+ 32 (* 1.8 c))))
+  (+ 32 (* 1.8 c)))
 
 (defn f-to-c [f]
-  (Math/round (/ (- f 32) 1.8)))
+  (/ (- f 32) 1.8))
+
+(defn c-to-emoji [c]
+  (cond (< c -30) '🥶
+        (< c 1) '⛄️
+        (< c 15) '🧣
+        (> c 40) '🔥
+        (> c 27) '🥵
+        :else '😀))
+
+(defn convert-temp [value current-unit desired-unit]
+  (if (= current-unit desired-unit)
+    value
+    (cond (= current-unit :fahrenheit) (f-to-c value)
+          (= current-unit :celcius) (c-to-f value))))
 
 (defn calc-temp [unit value]
-  (let [c (cond (= unit :fahrenheit) (f-to-c value)
-                :else value)
-        f (cond (= unit :celcius) (c-to-f value)
-                :else value)
-        emoji (cond (< c -30) '🥶
-                    (< c 1) '⛄️
-                    (< c 15) '🧣
-                    (> c 40) '🔥
-                    (> c 27) '🥵
-                    :else '😀)]
+  (let [c (convert-temp value unit :celcius)
+        f (convert-temp value unit :fahrenheit)
+        emoji (c-to-emoji c)]
     (hash-map :celcius c :fahrenheit f :emoji emoji)))
 
 (def temp-data (r/atom (calc-temp :celcius 0)))
 
 (defn slider [unit value min max]
   [:input {:type "range" :value value :min min :max max
-           :style {:width "100%"}
+           :class "w-full"
            :on-change (fn [e]
                         (let [new-value (js/parseInt (.. e -target -value))]
                           (swap! temp-data
@@ -37,16 +44,15 @@
     [:div
      [:h1 {:class "text-5xl font-bold mb-4"} [:span {:class "mr-2"} emoji] "Temperature Converter"]
      [:div
-      celcius "° Celcius"
+      (Math/round celcius) "° Celcius"
       [slider :celcius celcius -100 +100]]
      [:div
-      fahrenheit "° Fahrenheit"
+      (Math/round fahrenheit) "° Fahrenheit"
       [slider :fahrenheit fahrenheit -212 +212]]]))
 
 (defn template []
   [:div {:class "max-w-4xl mx-auto min-h-screen flex flex-col justify-center p-12"}
-   [temp-component]
-   ])
+   [temp-component]])
 
 (defn ^:dev/after-load start
   []
